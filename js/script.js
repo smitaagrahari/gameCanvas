c7292f1097cf83506b65b68dcb5ec613f39ef055
smitaagrahari <smikaagrahari@gmail.com>
2026-02-14 15:21:46 +0530

    started building base

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ const gameCanvas=document.getElementById("gameCanvas");
  gameCanvas.style.background="pink";
  const ctx=gameCanvas.getContext("2d");
 const gravity=0.5;
+// for player...
+const keys={
+    right:{
+        pressed:false
+    },
+    left:{
+        pressed:false
+    }
+
+}
  class Player{
     constructor(){
         this.position={
@@ -24,21 +34,63 @@ const gravity=0.5;
     update(){
         this.position.y+=this.velocity.y;
         this.position.x+=this.velocity.x;
-        if(this.position.y+this.height+this.velocity.y>=window.innerHeight){
-            this.velocity.y=0;
-            //   window.location.reload();
-        }
-        else{
-            this.velocity.y+=gravity;
-        }
+     if(this.position.y + this.height >= gameCanvas.height){
+        this.velocity.y = 0;
+        this.position.y = gameCanvas.height - this.height;
+    } else {
+        this.velocity.y += gravity;
+    }
+    // Right wall
+    if(this.position.x + this.width >= gameCanvas.width){
+        this.position.x = gameCanvas.width - this.width;
+    }
+
+    // Left wall
+    if(this.position.x <= 0){
+        this.position.x = 0;
+    }
+
     }
  }
+ // now players movement part 
+
  const player = new Player();
 function animate(){
     requestAnimationFrame(animate);
     ctx.clearRect(0,0,gameCanvas.width,gameCanvas.height);
+ if(keys.right.pressed){
+    player.velocity.x = 5;
+}
+else if(keys.left.pressed){
+    player.velocity.x = -5;
+}
+else{
+    player.velocity.x = 0;
+}
+
     player.draw();
 player.update();
    
 }
 animate();
+// eventlistener
+window.addEventListener("keydown",(event)=>{
+    if(event.key==="ArrowRight"){
+        keys.right.pressed=true;
+    }
+    if(event.key==="ArrowLeft"){
+        keys.left.pressed=true;
+    }
+    if(event.key==="ArrowUp"){
+        player.velocity.y=-11;
+    }
+
+});
+window.addEventListener("keyup",(event)=>{
+    if(event.key==="ArrowRight"){
+        keys.right.pressed=false;
+    }
+    if(event.key==="ArrowLeft"){
+        keys.left.pressed=false;
+    }
+});
